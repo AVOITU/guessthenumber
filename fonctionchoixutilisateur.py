01aab25baad9fb6758453dd832620bd1ae53f02c
Antoine Voituron <avoitu@orange.fr>
2023-08-13 21:26:28 +0200

    ajout du fichier code et du code pour les conditions préalables à la boucle

---
+++ fonctionchoixutilisateur.py
@@ -0,0 +1,5 @@
+ if demande<number:
+    print("le chiffre que vous avez choisi est plus petit que le chiffre choisi par l'ordinateur")
+    demande=int(input())
+  else:
+    print("le chiffre que vous avez choisi est plus grand que le chiffre choisi par l'ordinateur")
